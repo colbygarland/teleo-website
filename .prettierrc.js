0f6a5e2a6643b882e693f9d08ed855a09f34f86c
Colby Garland <colbygarland@gmail.com>
2024-02-13 15:45:40 -0700

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  insertPragma: false,
+  proseWrap: "preserve",
+};
