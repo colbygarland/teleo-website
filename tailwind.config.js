8ad6417abc02f81670ebf4c3ac3081be8e234f46
Colby Garland <colbygarland@gmail.com>
2024-02-17 15:18:31 -0700

    add product shot

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ['*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: '#edfefe',
+          100: '#d1fbfc',
+          200: '#a9f5f8',
+          300: '#6eebf2',
+          400: '#2cd7e4',
+          500: '#10bbca',
+          600: '#1096aa',
+          700: '#137182', // this is the main one that matches the Tresel branding
+          800: '#1a6170',
+          900: '#1a505f',
+          950: '#0b3541',
+        },
+      },
       fontFamily: {
         main: 'Inter, sans-serif',
       },
