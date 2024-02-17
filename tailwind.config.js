26dcf0f46131a9eabf5f147d6844d02999ca2736
Colby Garland <colbygarland@gmail.com>
2024-02-17 10:22:51 -0700

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: 'Inter, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
-
